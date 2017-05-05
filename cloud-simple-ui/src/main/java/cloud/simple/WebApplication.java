f627b7db7eeb7cbc927f201793f4fbefae3f82b3
张永阁 <bjzhangyongge@corp.netease.com>
2017-05-05 12:20:19 +0800

    cloud-simple-ui

---
+++ cloud-simple-ui/src/main/java/cloud/simple/WebApplication.java
@@ -0,0 +1,13 @@
+package cloud.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+}
